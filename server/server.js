9af65e5d5e6efc8bad5ed9963012e88f9dcebcfc
markhuang <markhuang286@gmail.com>
2019-01-18 14:51:46 -0500

    mofify package.json

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const path = require("path");
 
 const app = express();
 const publicPath = path.join(__dirname, "..", "dist");
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
 	res.sendFile(path.join(publicPath, "index.html"));
