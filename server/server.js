b6e19df7cbc04998fa05454a70cbaf4ce258fb69
markhuang <markhuang286@gmail.com>
2019-01-18 13:31:42 -0500

    mofify server.js

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 const publicPath = path.join(__dirname, "..", "dist");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
 	res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(port, () => console.log("server is running"));
+app.listen(port, () => console.log(`server is running with ${port}`));
